814e1b412149e72790a1b708ca8845b29083779b
EsOrVi_2 <csharp_eov@hotmail.com>
2019-08-29 17:39:29 -0500

    EO - SimpleContacts

--- ProyectoTransversalApp/ProyectoTransversalApp/MainPage.xaml.cs
+++ ProyectoTransversalApp/ProyectoTransversalApp/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace ProyectoTransversalApp
@@ -16,6 +17,100 @@ namespace ProyectoTransversalApp
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new ContactViewModel();
         }
     }
+    public class ContactViewModel
+    {
+        public Command SmsCommand { get; }
+        public Command EmailCommand { get; }
+        public Command PhoneCommand { get; }
+        public Command NavigateCommand { get; }
+        public Contact Contact { get; }
+        public ContactViewModel(Contact contact)
+        {
+            Contact = contact;
+        }
+        public ContactViewModel()
+        {
+            Contact = new Contact
+            {
+                Name = "Estivenson Ortega Villar",
+                Address = "Centro comercial caribe plaza",
+                City = "Cartagena",
+                State = "WA",
+                ZipCode = "98052",
+                Email = "eortega@facturecolombia.com",
+                PhoneNumber = "3017915843"
+            };
+
+            SmsCommand = new Command(async () => await ExecuteSmsCommand());
+            EmailCommand = new Command(async () => await ExecuteEmailCommand());
+            PhoneCommand = new Command(ExecutePhoneCommand);
+            NavigateCommand = new Command(async () => await ExecuteNavigateCommand());
+        }
+
+        async Task ExecuteSmsCommand()
+        {
+            try
+            {
+                await Sms.ComposeAsync(new SmsMessage(string.Empty, Contact.PhoneNumber));
+            }
+            catch (Exception ex)
+            {
+                ProcessException(ex);
+            }
+        }
+
+        async Task ExecuteEmailCommand()
+        {
+            try
+            {
+                string subject = "Envío dee SMS";
+                string body = "Hola buenas tades este es el correo mire";
+                await Email.ComposeAsync(subject, body, Contact.Email);
+            }
+            catch (Exception ex)
+            {
+                ProcessException(ex);
+            }
+        }
+
+        void ExecutePhoneCommand()
+        {
+            try
+            {
+                PhoneDialer.Open(Contact.PhoneNumber);
+            }
+            catch (Exception ex)
+            {
+                ProcessException(ex);
+            }
+        }
+
+        async Task ExecuteNavigateCommand()
+        {
+            try
+            {
+                await Map.OpenAsync(new Placemark
+                {
+                    Thoroughfare = Contact.Address,
+                    Locality = Contact.City,
+                    AdminArea = Contact.State,
+                    PostalCode = Contact.ZipCode
+                });
+            }
+            catch (Exception ex)
+            {
+                ProcessException(ex);
+            }
+        }
+
+        void ProcessException(Exception ex)
+        {
+            if (ex != null)
+                Application.Current.MainPage.DisplayAlert("Error", ex.Message, "OK");
+        }
+    }
+
 }
